a7be74e359972298c26a94411de46faa02751b92
Goreckaya <goreckayaolya76@gmail.com>
2022-06-04 09:56:23 +0700

    first commit

---
+++ app/src/main/java/com/example/pz_11_goreckaya/pz_11_goreckaya3.kt
@@ -0,0 +1,11 @@
+package com.example.pz_11_goreckaya
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pz_11_goreckaya3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pz11_goreckaya3)
+    }
+}
